c7d9cb0aff874ae57e88de9555df9e7e0326ea86
minjuuH <hwangmj1124@gmail.com>
2022-11-09 00:44:34 +0900

    시간 선택 화면 수정
    - 버튼 배치 수정

--- app/src/main/java/com/example/mysitreview/time_choice.java
+++ app/src/main/java/com/example/mysitreview/time_choice.java
@@ -13,9 +13,8 @@ public class time_choice extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_choice);
 
-        findViewById(R.id.button17).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.timechoice).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                //Toast.makeText(getApplicationContext(), "회원등록으로 이동합니다.", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(time_choice.this, time_booking_final.class));
             }
         });
