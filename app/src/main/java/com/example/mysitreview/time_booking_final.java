cf49c3a9a5943b783fc8c61d5fa28f67c34bd843
minjuuH <hwangmj1124@gmail.com>
2022-11-14 20:59:51 +0900

    최종 예약 화면
    클래스 파일에 레이아웃의 텍스트필드의 텍스트를 지정해주는 작업 추가
    추후 실제 데이터베이스의 데이터를 받아와 출력하도록 수정 필요

--- app/src/main/java/com/example/mysitreview/time_booking_final.java
+++ app/src/main/java/com/example/mysitreview/time_booking_final.java
@@ -3,6 +3,7 @@ package com.example.mysitreview;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +13,22 @@ public class time_booking_final extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_booking_final);
+
+        //텍스트필드 객체 받아옴
+        TextView bn = (TextView)findViewById(R.id.bookingname);
+        TextView bi = (TextView)findViewById(R.id.bookinginfo);
+        TextView nt = (TextView)findViewById(R.id.nametext);
+        TextView pt = (TextView)findViewById(R.id.phonetext);
+        TextView dt = (TextView)findViewById(R.id.datetext);
+        TextView tt = (TextView)findViewById(R.id.timetext);
+
+        //추후 데이터 연결 작업 필요
+        bn.setText("장소명");
+        bi.setText("장소 상세 정보");
+        nt.setText("예약자");
+        pt.setText("010-xxxx-xxxx");
+        dt.setText("20xx.xx.xx(일)");
+        tt.setText("00:00 - 00:00");
 
         findViewById(R.id.pbooking_btn).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
