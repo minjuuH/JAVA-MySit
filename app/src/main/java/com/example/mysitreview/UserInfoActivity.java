60e289a6f9fdb8e003ad4a23cf3971dd848eafde
minjuuH <hwangmj1124@gmail.com>
2022-11-21 16:42:13 +0900

    마이페이지 화면 수정
    - colors.xml : 수정 전 텍스트 색상 설정
    - UserInfoActivity : 수정 textView 클릭 시 비밀번호 입력 창이 생성 되는 작업 추가

--- app/src/main/java/com/example/mysitreview/UserInfoActivity.java
+++ app/src/main/java/com/example/mysitreview/UserInfoActivity.java
@@ -5,12 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class UserInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+
+        EditText username = (EditText)findViewById(R.id.username);
+        EditText useremail = (EditText)findViewById(R.id.useremail);
+        EditText userphone = (EditText)findViewById(R.id.userphone);
+        EditText userpw = (EditText)findViewById(R.id.userpw);
+
+        findViewById(R.id.reviseBnt).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Toast.makeText(getApplicationContext(), "수정 텍스트뷰 클릭되었습니다.", Toast.LENGTH_SHORT).show();
+
+                userpw.setEnabled(true);
+                userpw.setHintTextColor(getResources().getColor(R.color.text_gray));
+            }
+        });
 
         findViewById(R.id.logoutBtn).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
