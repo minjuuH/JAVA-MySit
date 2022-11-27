9b94831e46d2adaf1531d25b21a71760554446e1
minjuuH <hwangmj1124@gmail.com>
2022-11-27 18:05:31 +0900

    글 등록 화면 추가
    - java 액티비티 파일 추가
    - WriteFreePostActivity / WritePostActivity

---
+++ app/src/main/java/com/example/mysitreview/WriteFreePostActivity.java
@@ -0,0 +1,88 @@
+package com.example.mysitreview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.InputStream;
+
+
+public class WriteFreePostActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 0;
+    private final int SELECT_MOVIE = 2;
+    ImageView imageView;
+    Button button;
+
+    // 영상선택
+    private void doSelectMovie()
+    {
+        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+        i.setType("video/*");
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        try
+        {
+            startActivityForResult(i, SELECT_MOVIE);
+        } catch (android.content.ActivityNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_free_post);
+
+        imageView = findViewById(R.id.image);
+        button = (Button)findViewById(R.id.imagebutton);
+        // 이미지 선택 버튼
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+        // 확인 버튼
+        findViewById(R.id.check).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "저장되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+
+                    imageView.setImageBitmap(img);
+                } catch (Exception e) {
+
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+
+}
