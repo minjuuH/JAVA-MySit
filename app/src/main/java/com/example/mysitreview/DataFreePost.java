96950887eb637ec7ad3eee056b21405effe73e62
minjuuH <hwangmj1124@gmail.com>
2022-11-30 23:56:12 +0900

    Select 화면 recyclerview 작업
    - DataFreePost
    - 자유예약에 사용할 데이터 클래스 수정

--- app/src/main/java/com/example/mysitreview/DataFreePost.java
+++ app/src/main/java/com/example/mysitreview/DataFreePost.java
@@ -1,14 +1,18 @@
 package com.example.mysitreview;
 
-public class MainData {
+public class DataFreePost {
     private int pic;
     private String title;
+    private String introduce;
+    private String deadline;
     private String remain;
 
     //생성자
-    public MainData(int pic, String title, String remain) {
+    public DataFreePost(int pic, String title, String introduce, String deadline, String remain) {
         this.pic = pic;
         this.title = title;
+        this.introduce = introduce;
+        this.deadline = deadline;
         this.remain = remain;
     }
 
@@ -16,11 +20,11 @@ public class MainData {
     public int getPic() {
         return pic;
     }
-
     public String getTitle() {
         return title;
     }
-
+    public String getIntroduce() { return this.introduce; }
+    public String getDeadline() { return deadline; }
     public String getRemain() {
         return remain;
     }
@@ -29,11 +33,11 @@ public class MainData {
     public void setPic(int pic) {
         this.pic = pic;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
+    public void setIntroduce(String introduce) { this.introduce = introduce; }
+    public void setDeadline(String deadline) { this.deadline = deadline; }
     public void setRemain(String remain) {
         this.remain = remain;
     }
