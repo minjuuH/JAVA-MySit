bc6702a19d17572b5002fd84fba7a9bcbc03c66b
minjuuH <hwangmj1124@gmail.com>
2022-12-01 00:00:52 +0900

    Select 화면 recyclerview 작업
    - ShowWritedPost
    - 이전 액티비티에서 받아온 데이터를 출력

--- app/src/main/java/com/example/mysitreview/ShowWritedPost.java
+++ app/src/main/java/com/example/mysitreview/ShowWritedPost.java
@@ -3,6 +3,7 @@ package com.example.mysitreview;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,16 @@ public class ShowWritedPost extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_writed_post);
+
+        String title = getIntent().getStringExtra("title");
+        String intro = getIntent().getStringExtra("introduce");
+
+        TextView titleTextView = findViewById(R.id.titleEditText);
+        TextView introTextView = findViewById(R.id.introduce);
+
+        titleTextView.setText(title);
+        introTextView.setText(intro);
+
         findViewById(R.id.check).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(ShowWritedPost.this, time_choice_plus.class));
