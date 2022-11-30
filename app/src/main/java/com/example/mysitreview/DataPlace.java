58f62502d52c63b894a3076807c152e0471dfb07
minjuuH <hwangmj1124@gmail.com>
2022-11-30 23:53:01 +0900

    Select 화면 recyclerview 작업
    -

---
+++ app/src/main/java/com/example/mysitreview/DataPlace.java
@@ -0,0 +1,23 @@
+package com.example.mysitreview;
+
+public class DataPlace {
+    private int pic;
+    private String title;
+    private String introduce;
+
+    public DataPlace(int pic, String title, String introduce){
+        this.pic = pic;
+        this.title = title;
+        this.introduce = introduce;
+    }
+
+    //Getter
+    public int getPic() { return this.pic; }
+    public String getTitle() { return this.title; }
+    public String getIntroduce() { return this.introduce; }
+
+    //Setter
+    public void setPic(int pic) { this.pic = pic; }
+    public void setTitle(String title) { this.title = title; }
+    public void setIntroduce(String introduce) { this.introduce = introduce; }
+}
