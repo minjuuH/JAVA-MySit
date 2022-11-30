d951f49aedd8fcfabc7f26f84a72bb360ceb063c
minjuuH <hwangmj1124@gmail.com>
2022-11-30 23:59:17 +0900

    Select 화면 recyclerview 작업
    - SelectPlace
    - itemView에 데이터 연결
    - itemView를 클릭했을 때 해당 itemView의 데이터를 다음 액티비티에 전달

--- app/src/main/java/com/example/mysitreview/SelectPlace.java
+++ app/src/main/java/com/example/mysitreview/SelectPlace.java
@@ -8,25 +8,26 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 //import com.example.mysitreview.MyReCyclerAdapter;
 //리사이클러뷰 연습 코드
-//activity_sub와 연계
+//activity_select_place와 연계
 
 public class SelectPlace extends AppCompatActivity implements rvInterface {
 
-    private ArrayList<MainData> arrayList;
-    private MainAdapter mainAdapter;
+    private ArrayList<DataPlace> arrayList;
+    private AdapterPlace placeAdapter;
     private RecyclerView recyclerView;
     private LinearLayoutManager linearLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_place);
+        setContentView(R.layout.activity_select);
 
         findViewById(R.id.SelectButton).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -43,20 +44,38 @@ public class SelectPlace extends AppCompatActivity implements rvInterface {
         recyclerView.addItemDecoration(dividerItemDecoration);
 
         arrayList = new ArrayList<>();
-        mainAdapter = new MainAdapter(arrayList, this);
-        recyclerView.setAdapter(mainAdapter);       //설정한 adapter를 recyclerView에 설정
+        placeAdapter = new AdapterPlace(arrayList, this);
+        recyclerView.setAdapter(placeAdapter);       //설정한 adapter를 recyclerView에 설정
 
-        for(int i=0;i<10;i++){
-            MainData mainData = new MainData(R.mipmap.ic_launcher, "title", "잔여");
-            arrayList.add(mainData);
-            mainAdapter.notifyDataSetChanged();
+        String deadline[] = new String[]{"11-01 ~ 11-24", "11-10 ~ 11-20", "12-01 ~ 12-08", "11-11 ~ 11-24", "11-01 ~ 11-05"};
+
+        for(int i=0;i<5;i++){
+            DataPlace placeData = new DataPlace(R.mipmap.ic_launcher, "title"+Integer.toString(i),"상세정보"+Integer.toString(i));
+            arrayList.add(placeData);
+            placeAdapter.notifyDataSetChanged();
+        }
+
+        //출력할 데이터가 없을 경우
+        if(arrayList.size()==0){
+            //noneList textView 객체를 받아와 recyclerView 대신 보이게 설정
+            TextView noneText = (TextView) findViewById(R.id.noneList);
+            noneText.setVisibility(View.VISIBLE);
+            noneText.setEnabled(true);
+
+            //recyclerView를 화면에 보이지 않게 설정
+            recyclerView.setVisibility(View.GONE);
+            recyclerView.setEnabled(false);
         }
     }
 
     @Override
     public void onItemClick(int position) {
-        Intent subPage = new Intent(SelectPlace.this, ShowWritedPost.class);
-        startActivity(subPage);
+        Intent intent = new Intent(SelectPlace.this, ShowWritedPost.class);
+
+        intent.putExtra("title", arrayList.get(position).getTitle());
+        intent.putExtra("introduce", arrayList.get(position).getIntroduce());
+
+        startActivity(intent);
     }
 
     public void onButtonClick(View v) {
