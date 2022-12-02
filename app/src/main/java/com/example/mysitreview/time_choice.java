bb19abd3d49dba3e783bd3b2710eb1095b56d36f
minjuuH <hwangmj1124@gmail.com>
2022-12-02 18:18:09 +0900

    파일명 변경
    - time_booking_final -> FinalTimeBooking
    - 다른 파일에 연결된 부분 수정

--- app/src/main/java/com/example/mysitreview/time_choice.java
+++ app/src/main/java/com/example/mysitreview/time_choice.java
@@ -3,7 +3,6 @@ package com.example.mysitreview;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,7 +14,7 @@ public class time_choice extends AppCompatActivity {
 
         findViewById(R.id.timechoice).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                startActivity(new Intent(time_choice.this, time_booking_final.class));
+                startActivity(new Intent(time_choice.this, FinalTimeBooking.class));
             }
         });
     }
