216e153b0ea21486a606da6e25115d847758f132
minjuuH <hwangmj1124@gmail.com>
2022-12-02 18:06:04 +0900

    장소&자유 목록>작성글 확인
    - ShowFreeWritedPost
    - 자유 작성글 확인 액티비티 추가

---
+++ app/src/main/java/com/example/mysitreview/ShowFreeWritedPost.java
@@ -0,0 +1,32 @@
+package com.example.mysitreview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ShowFreeWritedPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_free_writed_post);
+
+        String title = getIntent().getStringExtra("title");
+        String intro = getIntent().getStringExtra("introduce");
+
+        TextView titleTextView = findViewById(R.id.titleEditText);
+        TextView introTextView = findViewById(R.id.introduce);
+
+        titleTextView.setText(title);
+        introTextView.setText(intro);
+
+        findViewById(R.id.check).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(ShowFreeWritedPost.this, FinalBooking.class));
+            }
+        });
+    }
+}
