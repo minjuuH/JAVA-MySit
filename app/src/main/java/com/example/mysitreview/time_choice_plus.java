1e67a620c010f6f7981749a18b248ff489202114
minjuuH <hwangmj1124@gmail.com>
2022-12-02 18:21:03 +0900

    시간 선택>최종예약화면에 데이터 전달
    - time_choice_plus
    - 현재 시간 선택 데이터가 안받아지는 오류 발생

--- app/src/main/java/com/example/mysitreview/time_choice_plus.java
+++ app/src/main/java/com/example/mysitreview/time_choice_plus.java
@@ -2,82 +2,87 @@ package com.example.mysitreview;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
-import android.widget.Chronometer;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class time_choice_plus extends AppCompatActivity {
+    String[] items = {"---시간 선택---", "00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"};
 
-    Chronometer chrono1;
-    Button btnStart, btnEnd;
     RadioButton rdoCal, rdoTime;
-    TimePicker tPicker1;
+    Button btnEnd;
+    Spinner spinner1;
     TextView tvResult;
     CalendarView calView1;
-
     int selectYear, selectMonth, selectDay;
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_choice_plus);
-        chrono1=findViewById(R.id.chrono1);
-        btnStart=findViewById(R.id.btnStart);
-        btnEnd=findViewById(R.id.btnEnd);
-        rdoCal=findViewById(R.id.rdoCal);
-        rdoTime=findViewById(R.id.rdoTime);
-        tPicker1=findViewById(R.id.tPicker1);
-        tvResult=findViewById(R.id.tvResult);
-        calView1=findViewById(R.id.calView1);
-
-        btnStart.setOnClickListener(v -> {
-            chrono1.setBase(SystemClock.elapsedRealtime());
-            chrono1.start();
-            chrono1.setTextColor(Color.RED);
-        });
+        btnEnd = findViewById(R.id.btnEnd);
+        rdoCal = findViewById(R.id.rdoCal);
+        rdoTime = findViewById(R.id.rdoTime);
+        spinner1 = findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner1.setAdapter(adapter);
+        tvResult = findViewById(R.id.tvResult);
+        calView1 = findViewById(R.id.calView1);
 
         rdoCal.setOnClickListener(v -> {
             calView1.setVisibility(View.VISIBLE);
-            tPicker1.setVisibility(View.INVISIBLE);
+            spinner1.setVisibility(View.INVISIBLE);
         });
 
         rdoTime.setOnClickListener(v -> {
             calView1.setVisibility(View.INVISIBLE);
-            tPicker1.setVisibility(View.VISIBLE);
+            spinner1.setVisibility(View.VISIBLE);
         });
 
-        btnEnd.setOnClickListener(v -> {
-            chrono1.stop();
-            chrono1.setTextColor(Color.BLACK);
-            tvResult.setText(selectYear+"년"+selectMonth+"월"+selectDay+"일"+tPicker1.getCurrentHour()+"시"+tPicker1.getCurrentMinute()+"분에 예약되었습니다.");
-
-            //값을 전달하기 위한 intent 생성
-            Intent intent = new Intent(time_choice_plus.this, time_booking_final.class);
-
-            //time_booking_final로 값 전달
-            intent.putExtra("년", selectYear); intent.putExtra("월", selectMonth); intent.putExtra("일", selectDay);
-            intent.putExtra("시", tPicker1.getCurrentHour()); intent.putExtra("분", tPicker1.getCurrentMinute());
-
-            //time_booking_final 시작
-            startActivity(intent);
+        calView1.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            selectYear = year;
+            selectMonth = month + 1;
+            selectDay = dayOfMonth;
         });
 
-       calView1.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
-           selectYear=year;
-           selectMonth=month+1;
-           selectDay=dayOfMonth;
-       });
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onItemSelected(AdapterView < ? > parent, View view, int position, long id){
 
+                if (selectYear == 0)
+                {
+                    btnEnd.setOnClickListener(v -> tvResult.setText("날짜를 선택해야 예약이 가능합니다."));
+                }
 
+                else {
+                    btnEnd.setOnClickListener(v -> tvResult.setText(selectYear + "년" + selectMonth + "월" + selectDay + "일 " + items[position] + "에 예약되었습니다."));
+
+                    //값을 전달하기 위한 intent 생성
+                    Intent intent = new Intent(time_choice_plus.this, FinalTimeBooking.class);
+
+                    //time_booking_final로 값 전달
+                    intent.putExtra("년", selectYear); intent.putExtra("월", selectMonth); intent.putExtra("일", selectDay);
+                    intent.putExtra("분", items[position]);
+
+                    //time_booking_final 시작
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onNothingSelected (AdapterView < ? > parent){
+                btnEnd.setOnClickListener(v -> tvResult.setText("시간을 선택해야 예약이 가능합니다."));
+            }});
     }
 }
