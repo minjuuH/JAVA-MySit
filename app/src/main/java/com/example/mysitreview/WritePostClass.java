f283397589895580e8b033073622b51205319f42
minjuuH <hwangmj1124@gmail.com>
2022-12-04 01:33:13 +0900

    장소목록> 장소글작성
    - WritePostClass
    - 작성한 장소글 정보를 저장할 class 생성

---
+++ app/src/main/java/com/example/mysitreview/WritePostClass.java
@@ -0,0 +1,50 @@
+package com.example.mysitreview;
+
+public class WritePostClass {
+    private String title;
+    private String introduce;
+    private String starttime;
+    private String endtime;
+    private String content;
+
+    public WritePostClass(){;}
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIntroduce() {
+        return introduce;
+    }
+
+    public void setIntroduce(String introduce) {
+        this.introduce = introduce;
+    }
+
+    public String getStarttime() {
+        return starttime;
+    }
+
+    public void setStarttime(String starttime) {
+        this.starttime = starttime;
+    }
+
+    public String getEndtime() {
+        return endtime;
+    }
+
+    public void setEndtime(String endtime) {
+        this.endtime = endtime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
