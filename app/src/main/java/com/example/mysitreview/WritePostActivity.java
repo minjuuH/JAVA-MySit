17b490402a719ff4c7c1d2ce7978e0acd07e597b
minjuuH <hwangmj1124@gmail.com>
2022-12-04 01:34:30 +0900

    장소목록> 장소글작성
    - WritePostActivity
    - 작성글 정보가 저장될 실시간 데이터베이스 연결

--- app/src/main/java/com/example/mysitreview/WritePostActivity.java
+++ app/src/main/java/com/example/mysitreview/WritePostActivity.java
@@ -7,15 +7,20 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.InputStream;
 
 
 
 public class WritePostActivity extends AppCompatActivity {
+    private DatabaseReference mDatabaseRef; // 실시간 데이터베이스
     private static final int REQUEST_CODE = 0;
     private final int SELECT_MOVIE = 2;
     ImageView imageView;
@@ -53,9 +58,22 @@ public class WritePostActivity extends AppCompatActivity {
                 startActivityForResult(intent, REQUEST_CODE);
             }
         });
+        EditText Title = findViewById(R.id.titleEditText);
+        EditText Introduce = findViewById(R.id.tv_introduce);
+        EditText Opentime = findViewById(R.id.openTime);
+        EditText Endtime = findViewById(R.id.endTime);
+        EditText Content = findViewById(R.id.contenttext);
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("장소 글작성 정보");
         // 확인 버튼
         findViewById(R.id.check).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                WritePostClass wp = new WritePostClass();
+                wp.setTitle(Title.getText().toString());
+                wp.setIntroduce(Introduce.getText().toString());
+                wp.setStarttime(Opentime.getText().toString());
+                wp.setEndtime(Endtime.getText().toString());
+                wp.setContent(Content.getText().toString());
+                mDatabaseRef.child(Title.getText().toString()).setValue(wp);
                 Toast.makeText(getApplicationContext(), "저장되었습니다.", Toast.LENGTH_SHORT).show();
             }
         });
@@ -66,6 +84,11 @@ public class WritePostActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE) {
+            // 파이어 스토어에
+//            Uri file = data.getData();
+//            StorageReference storageRef = storage.getReference();
+//            StorageReference riversRef = storageRef.child("photo/1.png");
+//            UploadTask uploadTask = riversRef.putFile(file);
             if (resultCode == RESULT_OK) {
                 try {
                     InputStream in = getContentResolver().openInputStream(data.getData());
@@ -82,7 +105,4 @@ public class WritePostActivity extends AppCompatActivity {
             }
         }
     }
-
-
-
 }
