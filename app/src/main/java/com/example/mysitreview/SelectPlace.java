f4e0af9d264b127d1f3f2f5055500368b74959a4
minjuuH <hwangmj1124@gmail.com>
2022-12-04 01:40:53 +0900

    장소목록> 장소정보확인
    - SelectPlace
    - 실시간데이터베이스 연결
    - 새로운 어댑터 PlaceAdapter 연결

--- app/src/main/java/com/example/mysitreview/SelectPlace.java
+++ app/src/main/java/com/example/mysitreview/SelectPlace.java
@@ -1,5 +1,6 @@
 package com.example.mysitreview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -8,85 +9,105 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.SearchView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 //리사이클러뷰 연습 코드
 //activity_select_place와 연계
-
 public class SelectPlace extends AppCompatActivity implements rvInterface {
-
-    private ArrayList<DataPlace> arrayList;
-    private AdapterPlace placeAdapter;
-    private RecyclerView recyclerView;
-    private LinearLayoutManager linearLayoutManager;
+    RecyclerView recyclerView;
+    DatabaseReference mDatabaseRef; // 실시간 데이터베이스
+    PlaceAdapter placeAdapter;
+    ArrayList<DataPlace> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
-        //findViewById(R.id.search_view).setOnQueryTextListener(object : search_view.OnQueryTextListener{
 
-            //override fun onQueryTextChange(newText: String?): Boolean {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
 
-                //검색어 입력 순간마다의 이벤트...
+        list = new ArrayList<>();
+        placeAdapter = new PlaceAdapter(list,this, this); //*
+        //placeAdapter = new AdapterPlace(list, this);
+        recyclerView.setAdapter(placeAdapter);
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("장소 글작성 정보");
+        mDatabaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    DataPlace dp = dataSnapshot.getValue(DataPlace.class);
+                    list.add(dp);
+                }
+                placeAdapter.notifyDataSetChanged();
+            }
 
-             //   return true
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+//        // 아이템 선택
+//        public void onItemClick(int position) {
+//            Intent intent = new Intent(SelectPlace.this, ShowWritedPost.class);
+//            intent.putExtra("title", list.get(position).getTitle());
+//
+//            startActivity(intent);
+//        }
+        //Divider(구분선) 추가
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, linearLayoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
 
-         //   }
+        // 글작성으로 이동
         findViewById(R.id.SelectButton).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(SelectPlace.this, WritePostActivity.class));
             }
         });
-
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        linearLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
-
-        //Divider 추가
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, linearLayoutManager.getOrientation());
-        recyclerView.addItemDecoration(dividerItemDecoration);
-
-        arrayList = new ArrayList<>();
-        placeAdapter = new AdapterPlace(arrayList, this);
-        recyclerView.setAdapter(placeAdapter);       //설정한 adapter를 recyclerView에 설정
-
-        String deadline[] = new String[]{"11-01 ~ 11-24", "11-10 ~ 11-20", "12-01 ~ 12-08", "11-11 ~ 11-24", "11-01 ~ 11-05"};
-
-        for(int i=0;i<5;i++){
-            DataPlace placeData = new DataPlace(R.mipmap.ic_launcher, "title"+Integer.toString(i),"상세정보"+Integer.toString(i));
-            arrayList.add(placeData);
-            placeAdapter.notifyDataSetChanged();
-        }
-
-        //출력할 데이터가 없을 경우
-        if(arrayList.size()==0){
-            //noneList textView 객체를 받아와 recyclerView 대신 보이게 설정
-            TextView noneText = (TextView) findViewById(R.id.noneList);
-            noneText.setVisibility(View.VISIBLE);
-            noneText.setEnabled(true);
-
-            //recyclerView를 화면에 보이지 않게 설정
-            recyclerView.setVisibility(View.GONE);
-            recyclerView.setEnabled(false);
-        }
     }
 
     @Override
     public void onItemClick(int position) {
         Intent intent = new Intent(SelectPlace.this, ShowWritedPost.class);
 
-        intent.putExtra("title", arrayList.get(position).getTitle());
-        intent.putExtra("introduce", arrayList.get(position).getIntroduce());
-
+        intent.putExtra("title", list.get(position).getTitle());
+        intent.putExtra("introduce", list.get(position).getIntroduce());
         startActivity(intent);
     }
-
-    public void onButtonClick(View v) {
-        Toast.makeText(getApplicationContext(), "글 등록으로 이동합니다.", Toast.LENGTH_SHORT).show();
-        startActivity(new Intent(SelectPlace.this, make_reserv_post.class));
-    }
 }
+        //findViewById(R.id.search_view).setOnQueryTextListener(object : search_view.OnQueryTextListener{
+
+        //override fun onQueryTextChange(newText: String?): Boolean {
+
+        //검색어 입력 순간마다의 이벤트...
+
+        //   return true
+
+        //   }
+
+        //String deadline[] = new String[]{"11-01 ~ 11-24", "11-10 ~ 11-20", "12-01 ~ 12-08", "11-11 ~ 11-24", "11-01 ~ 11-05"};
+
+        //출력할 데이터가 없을 경우 => 활성화하면 리스트뷰가 안나와서 주석처리
+//        if(list.size()==0){
+//            //noneList textView 객체를 받아와 recyclerView 대신 보이게 설정
+//            TextView noneText = (TextView) findViewById(R.id.noneList);
+//            noneText.setVisibility(View.VISIBLE);
+//            noneText.setEnabled(true);
+//            //recyclerView를 화면에 보이지 않게 설정
+//            recyclerView.setVisibility(View.GONE);
+//            recyclerView.setEnabled(false);
+//        }
+
