9b08a40ecd35e5031a95f87533288bfd52a109bc
minjuuH <hwangmj1124@gmail.com>
2022-12-04 17:24:23 +0900

    장소> 최종예약확인
    - FinalTimeBooking
    - 데이터베이스 연결 -> pbooking_btn 클릭 시 예약정보 데이터베이스에 저장

--- app/src/main/java/com/example/mysitreview/FinalTimeBooking.java
+++ app/src/main/java/com/example/mysitreview/FinalTimeBooking.java
@@ -7,13 +7,21 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class FinalTimeBooking extends AppCompatActivity {
-    private DatabaseReference mDatabaseRef; // 실시간 데이터베이스
+    private DatabaseReference mDatabaseRefUser; // 실시간 데이터베이스
+    private DatabaseReference mDatabaseRefPlace; // 실시간 데이터베이스
+    private FirebaseAuth mFirebaseAuth;     // 파이어베이스 인증
     //두자리 서식 지정을 위한 함수 지정
     public String format(int n){
         if(n<10)
@@ -24,7 +32,6 @@ public class FinalTimeBooking extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_booking_final);
-        mDatabaseRef = FirebaseDatabase.getInstance().getReference("장소 글작성 정보");
 
         //이미지 출력
         ImageView imageView = findViewById(R.id.imageView);
@@ -38,8 +45,23 @@ public class FinalTimeBooking extends AppCompatActivity {
         TextView dt = (TextView)findViewById(R.id.datetext);
         TextView tt = (TextView)findViewById(R.id.timetext);
 
-        Intent intent = getIntent();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRefUser = FirebaseDatabase.getInstance().getReference("myseat");
+        mDatabaseRefPlace = FirebaseDatabase.getInstance().getReference("장소 글작성 정보");
+        FirebaseUser User = mFirebaseAuth.getCurrentUser(); // 회원가입된 유저 가져옴
+        mDatabaseRefUser.child("UserAccount").child(User.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                FinalPlaceReservation fpr = snapshot.getValue(FinalPlaceReservation.class);
+                nt.setText(fpr.getName());
+                pt.setText(fpr.getPhNum());
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
 
+        Intent intent = getIntent();
         //intet에서 받아온 값의 출력 서식 지정
         String dateText = Integer.toString(intent.getIntExtra("년", 0))+"-"+format(intent.getIntExtra("월", 0))
                 +"-"+format(intent.getIntExtra("일", 0));
@@ -48,15 +70,20 @@ public class FinalTimeBooking extends AppCompatActivity {
         //추후 데이터 연결 작업 필요
         bn.setText("장소명");
         bi.setText("장소 상세 정보");
-        nt.setText("예약자");
-        pt.setText("010-xxxx-xxxx");
         dt.setText(dateText);
         tt.setText(timeText);
 
         findViewById(R.id.pbooking_btn).setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "예약 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                FinalPlaceReservation fpr = new FinalPlaceReservation();
+                fpr.setName(nt.getText().toString());
+                fpr.setPhNum(pt.getText().toString());
+                fpr.setDay(dateText);
+                fpr.setTime(tt.getText().toString());
 
+                mDatabaseRefUser.child("예약정보").child(User.getUid()).push().setValue(fpr);
+
+                Toast.makeText(getApplicationContext(), "예약 완료되었습니다.", Toast.LENGTH_SHORT).show();
                 //엑티비티 하나를 제외하고 나머지 히스토리를 모두 지우는 코드
                 Intent i = new Intent(FinalTimeBooking.this, choice_menu.class);
                 i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
