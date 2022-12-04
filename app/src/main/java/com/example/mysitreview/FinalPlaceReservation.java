ede2ccf51c0984f7b73f3885a17ef9ec8ae6c198
minjuuH <hwangmj1124@gmail.com>
2022-12-04 17:22:24 +0900

    장소> 최종예약확인
    - FinalPlaceReservation.java
    - 최종예약정보를 저장할 클래스 생성

---
+++ app/src/main/java/com/example/mysitreview/FinalPlaceReservation.java
@@ -0,0 +1,64 @@
+package com.example.mysitreview;
+
+public class FinalPlaceReservation {
+    private String name;
+    private String phNum;
+
+    public String getPhNum() {
+        return phNum;
+    }
+
+    public void setPhNum(String phNum) {
+        this.phNum = phNum;
+    }
+
+    private String place;
+    private String introduce;
+    private String day;
+
+    public FinalPlaceReservation(){;}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getIntroduce() {
+        return introduce;
+    }
+
+    public void setIntroduce(String introduce) {
+        this.introduce = introduce;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    private String time;
+
+}
