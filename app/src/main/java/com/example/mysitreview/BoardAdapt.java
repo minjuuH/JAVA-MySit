3db6fa5c44a5f0bdd2d46f918fd3b3604007a431
minjuuH <hwangmj1124@gmail.com>
2022-12-05 15:45:49 +0900

    자유> 데이터를 담을 클래스 파일 생성
    - Board_Show.java
    - SelectFreePost에 사용할 데이터클래스 생성

---
+++ app/src/main/java/com/example/mysitreview/BoardAdapt.java
@@ -0,0 +1,33 @@
+package com.example.mysitreview;
+
+public class BoardAdapt {
+    private String title;
+    private String etime;
+    private String max_popul;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMax_popul() {
+        return max_popul;
+    }
+
+    public void setMax_popul(String max_popul) {
+        this.max_popul = max_popul;
+    }
+
+    public String getEtime() {
+        return etime;
+    }
+
+    public void setEtime(String etime) {this.etime = etime;}
+
+
+
+
+}
