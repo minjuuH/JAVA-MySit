96941bb2c4cbb88ddfd678d53a51f697b7ca359b
minjuuH <hwangmj1124@gmail.com>
2022-12-05 21:01:08 +0900

    자유목록> 자유정보확인
    - ShowFreeWritedPost
    - 데이터가 알맞은 textview로 들어가도록 수정
    - 현재 최소 인원이 null값으로 저장되어있는 오류 발생. 수정 필요

--- app/src/main/java/com/example/mysitreview/ShowFreeWritedPost.java
+++ app/src/main/java/com/example/mysitreview/ShowFreeWritedPost.java
@@ -22,24 +22,29 @@ public class ShowFreeWritedPost extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_free_writed_post);
 
-        Intent intent = new Intent();
+        //Intent intent = new Intent();
         String title = getIntent().getStringExtra("title");
 
         TextView titletv = findViewById(R.id.titleEditText);
-        TextView contenttv = findViewById(R.id.tv_introduce);
+        TextView introtv = findViewById(R.id.tv_introduce);
         TextView max_popultv = findViewById(R.id.nowcount);
         TextView stimetv = findViewById(R.id.tv_openTime);
         TextView etimetv = findViewById(R.id.tv_endTime);
+        TextView contenttv = findViewById(R.id.tv_content);
+        TextView min_popultv = findViewById(R.id.mincount);
 
         mDatabaseRef = FirebaseDatabase.getInstance().getReference("board");
         mDatabaseRef.child(title).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 Board_Show bs = snapshot.getValue(Board_Show.class);
+                titletv.setText(bs.getTitle());
+                introtv.setText(bs.getIntro());
                 contenttv.setText(bs.getContent());
                 max_popultv.setText("최대인원 : "+bs.getMax_popul());
                 stimetv.setText(bs.getStime());
                 etimetv.setText(bs.getEtime());
+                min_popultv.setText(String.format("%s(명) 이상 모집될 경우 확정됩니다!", bs.getMin_popul()));
             }
 
             @Override
@@ -47,7 +52,7 @@ public class ShowFreeWritedPost extends AppCompatActivity {
 
             }
         });
-        titletv.setText(title);
+        //titletv.setText(title);
 
 
 
