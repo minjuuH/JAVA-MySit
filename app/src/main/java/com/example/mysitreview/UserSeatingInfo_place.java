5bcce36fd472cb0c7cd2702527ac2f6c41f4c1c6
minjuuH <hwangmj1124@gmail.com>
2022-12-05 22:42:24 +0900

    마이페이지> 예약 내역
    - UserSeatingInfo_free
    - UserSeatingInfo_place
    - 예약상세정보 액티비티 추가
    - 하단 버튼 예약 취소 버튼으로 변경

---
+++ app/src/main/java/com/example/mysitreview/UserSeatingInfo_place.java
@@ -0,0 +1,39 @@
+package com.example.mysitreview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserSeatingInfo_place extends AppCompatActivity {
+    //장소 예약 상세정보 액티비티
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_final);
+
+        Intent intent = getIntent();
+
+        TextView bn = (TextView)findViewById(R.id.bookingname);
+        TextView dt = (TextView)findViewById(R.id.datetext);
+
+        bn.setText(intent.getStringExtra("title"));
+        dt.setText(intent.getStringExtra("sdate"));
+
+
+        Button cancleBnt = (Button) findViewById(R.id.booking_btn);
+        cancleBnt.setText("예약 취소");
+
+        cancleBnt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //추후 실제 취소 기능 연결 필요
+                Toast.makeText(getApplicationContext(), "예약 취소되었습니다.", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(UserSeatingInfo_place.this, UserInfoActivity.class));
+            }
+        });
+    }
+}
