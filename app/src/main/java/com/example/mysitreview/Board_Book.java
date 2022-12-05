6ce0b00a9989d23b8450a2f76e96964f8de59d3f
minjuuH <hwangmj1124@gmail.com>
2022-12-05 15:46:56 +0900

    자유> 데이터를 담을 클래스 파일 생성
    - Board_Book.java
    - FinalBooking에 사용할 데이터클래스 생성

---
+++ app/src/main/java/com/example/mysitreview/Board_Book.java
@@ -0,0 +1,87 @@
+package com.example.mysitreview;
+
+public class Board_Book {
+    private String  title;
+    private String  content;
+    private String  name;
+    private String  phNum;
+    private String  rtime;
+    private String  dtime;
+    private String  date;
+    private String  uid;
+
+    public Board_Book() {
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getPhNum() {
+        return phNum;
+    }
+
+    public void setPhNum(String phNum) {
+        this.phNum = phNum;
+    }
+
+
+    public String getRtime() {
+        return rtime;
+    }
+
+    public void setRtime(String rtime) {
+        this.rtime = rtime;
+    }
+
+
+    public String getDtime() {
+        return dtime;
+    }
+
+    public void setDtime(String dtime) {
+        this.dtime = dtime;
+    }
+
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+}
