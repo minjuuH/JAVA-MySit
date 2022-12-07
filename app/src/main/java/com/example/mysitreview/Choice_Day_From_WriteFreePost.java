f3d329faa1b3712339c8723b2a01594841055762
minjuuH <hwangmj1124@gmail.com>
2022-12-07 16:49:55 +0900

    자유> 날짜선택->글작성 순서 변경
    - Choice_Day_From_WriteFreePost
    - 날짜 데이터 추출 및 전달 기능 연결

--- app/src/main/java/com/example/mysitreview/Choice_Day_From_WriteFreePost.java
+++ app/src/main/java/com/example/mysitreview/Choice_Day_From_WriteFreePost.java
@@ -1,42 +1,66 @@
 package com.example.mysitreview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 
 public class Choice_Day_From_WriteFreePost extends AppCompatActivity {
 
-    Button add;
-    CalendarView calendarView;
-    TextView textView;
+    CalendarView calendarView1, calendarView2;
+    TextView today1, today2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice_day_from_write_free_post);
-        add = (Button)findViewById(R.id.button7);
-        calendarView = (CalendarView)findViewById(R.id.calendar);//캘린더뷰
-        add.setOnClickListener(new View.OnClickListener() {
+        today1 = findViewById(R.id.TVchdate);
+        today2 = findViewById(R.id.TVchdate2);
+        calendarView1 = findViewById(R.id.calendar);//캘린더뷰
+        calendarView2 = findViewById(R.id.calendar2);//캘린더뷰
+    DateFormat formatter = new SimpleDateFormat("yyyy년MM월dd일");
+        Date date1 = new Date(calendarView1.getDate());
+        Date date2 = new Date(calendarView2.getDate());
+        today1.setText(formatter.format(date1));
+        today2.setText(formatter.format(date2));
+        calendarView1.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onClick(View view) {
-                //현재의 날짜와 시간을 가지고 있는 캘린더 객체 생성
-                Calendar calendar = Calendar.getInstance();
-                //캘린더뷰에서 날짜값 읽어오기
-                Date date = new Date(calendarView.getDate());
-                //캘린더 객체에 캘린더뷰 값을 넣음
-                calendar.setTime(date);
-                //캘린더뷰 값을 넣은 캘린더 객체의 날짜값을 문자열로 변환!
-                // ex)2021-03-14
-                String caldate = Integer.toString(calendar.get(Calendar.YEAR))+ "-" + Integer.toString(calendar.get(Calendar.MONTH)) + "-" +Integer.toString(calendar.get(Calendar.DATE));
-                textView.setText(caldate);
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String day;
+                day = year + "년" + (month+1) + "월" + dayOfMonth +"일";
+                today1.setText(day);
+            }
+        });
+        calendarView2.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String day;
+                day = year + "년" + (month+1) + "월" + dayOfMonth +"일";
+                today2.setText(day);
+            }
+        });
+        findViewById(R.id.button7).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String date1, date2;
+                date1 = today1.getText().toString();
+                date2 = today2.getText().toString();
+                Toast.makeText(getApplicationContext(), "날이 선택되었습니다.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Choice_Day_From_WriteFreePost.this, WriteFreePostActivity.class);
+                intent.putExtra("StartDate", date1);
+                intent.putExtra("EndDate", date2);
+                startActivity(intent);
             }
         });
     }
